32037c1280e15e478879cdcbdd44af88c11f3b53
Hasan <hasanozdemir1@trakya.edu.tr>
2022-01-26 18:59:50 +0300

    unused methods is deleted

--- src/main/java/mka/root/RootApplication.java
+++ src/main/java/mka/root/RootApplication.java
@@ -10,16 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController // this class is a request handler (a REST controller).
 public class RootApplication {
 
+    // accept connection http://localhost:8080
     public static void main(String[] args) {
         SpringApplication.run(RootApplication.class, args);
-    }
-
-    // accept connection http://localhost:8080/index or http://localhost:8080
-    // maps the indexPage to GET requests for / and /index
-    @GetMapping({"/","/index"})
-    // RequestParam annotation maps the name method to the myName
-    public String indexPage(@RequestParam(value="myName", defaultValue = "Turkey") String name){
-        return String.format("Hello %s!", name);
     }
 
 }
