0d1d49b059c86d24ea760bd8a9a07e2d7efdd75a
Hasan <hasanozdemir1@trakya.edu.tr>
2022-01-27 00:44:53 +0300

    Content updates. Basic changes

--- src/main/java/mka/root/exceptions/ProductNotFoundException.java
+++ src/main/java/mka/root/exceptions/ProductNotFoundException.java
@@ -4,6 +4,6 @@ public class ProductNotFoundException extends Exception {
 
     private Integer emp_id;
     public ProductNotFoundException(Integer product_id) {
-        super(String.format("Employee is not found with id : '%s'", product_id));
+        super(String.format("Product is not found with id : '%s'", product_id));
     }
 }
